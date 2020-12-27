2d186f92c8fb1398184d4dabb36b303a00cd69fa
spandanajavvaji <75241362+spandanajavvaji@users.noreply.github.com>
2020-12-27 17:48:37 +0530

    Add files via upload

---
+++ selenium/Activity11_1.java
@@ -0,0 +1,29 @@
+package Selenium;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity11_1 
+{
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new FirefoxDriver();		
+		driver.get("https://www.training-support.net/selenium/javascript-alerts");		
+		driver.findElement(By.cssSelector("button#simple")).click();
+
+		//Switch to alert window
+		Alert simpleAlert = driver.switchTo().alert();
+
+		//Get text in the alert box and print it
+		String alertText = simpleAlert.getText();
+		System.out.println("Alert text is: " + alertText);
+
+		//Close the alert box
+		simpleAlert.accept();
+
+		//Close the Browser
+		driver.close();
+	}
+}
