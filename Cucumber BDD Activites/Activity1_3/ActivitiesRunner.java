efb450739e3bfd32404aad3e153a68fb6f2884a6
spandanajavvaji <75241362+spandanajavvaji@users.noreply.github.com>
2021-01-30 14:50:47 +0530

    Add files via upload

---
+++ Cucumber BDD Activites/Activity1_3/ActivitiesRunner.java
@@ -0,0 +1,19 @@
+package CucumberTest;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/java/Features",
+    glue = {"StepDefinitions"},
+    tags = "@activity1_3",
+    strict = true,
+    plugin = {"pretty", "html:Test_Reports/Cucumber_Project_Report.html","json:Test_Reports/Cucumber_Project_Report.json"},
+    monochrome = true
+)
+
+public class ActivitiesRunner {
+
+}
